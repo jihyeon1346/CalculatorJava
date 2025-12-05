ec1424910a667648f816b4817132cdb111f4ff6e
jihyeon <yio1346@gmail.com>
2025-12-05 09:56:22 +0900

    필수기능 Step1  내용 구현 완료

--- src/calculator/Step1.java
+++ src/calculator/Step1.java
@@ -12,33 +12,49 @@ public class Step1 {    public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
 
-    while (true) {
-        try {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            num1 = scanner.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("숫자를 입력해 주세요.");
-        }
-        scanner.nextLine();
+    while (true)//계산 반복을 위해 추가
+    {
+        while (true)// 원하는 입력값을 받기까지 반복
+        {
+            try  //숫자 이외의 입력 예외처리
+            {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                num1 = scanner.nextInt();
+                break;
+            } catch (InputMismatchException e)
+            {
+                System.out.println("숫자를 입력해 주세요.");
+            }
 
-        while (true) {
+            scanner.nextLine(); // enter 키 입력방지용
+
+        }
+
+        while (true)
+        {
             System.out.print("연산자를 입력하세요 (+, -, *, /) : ");
             operator = scanner.next().charAt(0);
-            if (operator == '+' || operator == '-' || operator == '*' || operator == '/')
+            if (operator == '+' || operator == '-' || operator == '*' || operator == '/')   //지정해놓은 연산자만 입력받기
                 break;
             else
                 System.out.println("지원하지 않는 연산자입니다. 다시 입력하세요.");
         }
-
-        try {
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            num2 = scanner.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("숫자를 입력해 주세요.");
+        while (true)
+        {
+            try
+            {
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                num2 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } catch (InputMismatchException e)
+            {
+                System.out.println("숫자를 입력해 주세요.");
+            }
+            scanner.nextLine();
         }
-        scanner.nextLine();
-
-        switch (operator) {
+        switch (operator)
+        {
             case '+':
                 result = num1 + num2;
                 break;
@@ -49,8 +65,10 @@ public class Step1 {    public static void main(String[] args) {
                 result = num1 * num2;
                 break;
             case '/':
-                if (num2 == 0) {
+                if (num2 == 0)
+                {
                     System.out.println("0으로 나눌 수 없습니다.");
+                    continue;
                 }
                 result = num1 / num2;
                 break;
@@ -59,10 +77,12 @@ public class Step1 {    public static void main(String[] args) {
         System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
         System.out.println("계산을 종료 하시려면 exit를 입력해주세요");
         exit = scanner.nextLine();
-        if (exit.equals("exit")) {
+        if (exit.equals("exit"))
+        {
             System.out.println("계산을 종료합니다.");
             System.exit(0);
-        } else {
+        } else
+        {
             System.out.println("계산을 이어서 진행합니다.");
         }
     }
