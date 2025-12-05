1c989682b310ceba6f96d5488bcd148c18143a5e
jihyeon <yio1346@gmail.com>
2025-12-05 10:31:34 +0900

    클래스 생성

---
+++ src/calculator/Step2.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import java.util.ArrayList;
+
+public class Step2
+{
+    int result;
+    ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+    public int calculate(String operator, int num1 , int num2)
+    {
+        return result;
+    }
+}
